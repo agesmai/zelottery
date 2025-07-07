a0bb70171a2e8bbfbde594d5a391bd94167a1f88
agesmai <anhpv1602@gmail.com>
2025-07-07 23:10:46 +0700

    Merge remote-tracking branch 'origin/main'

---
+++ src/environments/environment_sample.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "",
+    authDomain: "zelottery.firebaseapp.com",
+    projectId: "zelottery",
+    storageBucket: "zelottery.firebasestorage.app",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  }
+};
